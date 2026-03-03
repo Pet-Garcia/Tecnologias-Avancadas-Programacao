0061527a8d41783618598ffe815bd60e5c0746f3
Pet-Garcia <petrusgarcia11@gmail.com>
2026-03-03 14:33:25 -0300

    Add files via upload

--- Atividades/TPA-03_03/src/Aluno.java
+++ Atividades/TPA-03_03/src/Aluno.java
@@ -1,12 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author aluno
- */
 public class Aluno {
+    // atributos
+    String nome;
+    String dataNascimento;
+    String cpf;
+    String ra;
     
+    
+    // métodos --cad(argumentos servem para se comunicar entre classes)//pegar variaveis de outras classes
+    public void cadastrar(String n, String d, String c, String r){
+        nome = n;
+        dataNascimento = d;
+        cpf = c;
+        ra = r;
+    }
 }
