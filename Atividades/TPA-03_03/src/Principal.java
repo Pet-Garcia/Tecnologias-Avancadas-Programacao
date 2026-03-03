c2be96b19cd1ea7352322c69d1ae52c244489704
Pet-Garcia <petrusgarcia11@gmail.com>
2026-03-03 14:51:36 -0300

    Atualização da aula de 03/03 de TAP

--- Atividades/TPA-03_03/src/Principal.java
+++ Atividades/TPA-03_03/src/Principal.java
@@ -1,6 +1,24 @@
+
+import javax.swing.JOptionPane;
+
 public class Principal {
-    public static void main(String args[]){
+    public static void main(String args[])/*argumentos; args[], ... tudo antes do [] são variaveis; []args, ... tudo depois do [] são vetores */{
         
-        System.out.println("");
+        Aluno a = new Aluno(); // Aluno se torna um tipo, semelhante ao tipo int; significa que é mecessário ter um nome Aluno a.nome = "zé";
+                 // o nome a se torna um objeto; = new Aluno() está instanciando o objeto para tornar ele existente
+                 
+        String nome, data, cpf, ra;
+/*
+        nome = "Zé";
+        data = "10/03/2006";
+        cpf = "49323833";
+        ra = "25532";
+*/
+
+        nome = JOptionPane.showInputDialog("Digite o nome");
+        data = JOptionPane.showInputDialog("Digite a data de nascimento");
+        cpf = JOptionPane.showInputDialog("Digite o cpf");
+        ra = JOptionPane.showInputDialog("Digite o ra");
+        
     }
 }
