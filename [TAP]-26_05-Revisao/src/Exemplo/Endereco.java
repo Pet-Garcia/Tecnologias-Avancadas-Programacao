3a72c8d1f011dfd24922bc078d566a5f0120d2cb
Pet-Garcia <petrusgarcia11@gmail.com>
2026-05-26 16:09:52 -0300

    Adicionado o projeto de revisão de Herança e Associação

---
+++ [TAP]-26_05-Revisao/src/Exemplo/Endereco.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exemplo;
+
+/**
+ *
+ * @author aluno
+ */
+public class Endereco {
+    private String logradouro;
+    private String numero;
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+}
---
+++ [TAP]-26_05-Revisao/src/Exemplo/Pessoa.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exemplo;
+
+/**
+ *
+ * @author aluno
+ */
+public class Pessoa {
+    private String nome;
+    private Endereco endereco; // Associação
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
